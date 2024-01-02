3760e364961e518a1adc7eea3b3fa24f7031afca
Teja Kummarikuntla <teja.kummarikuntla@harness.io>
2024-01-02 14:48:55 +0530

    inital commit

---
+++ src/components/HelpArea.jsx
@@ -0,0 +1,32 @@
+import HelpBox from './HelpBox';
+import './HelpArea.css';
+
+const HELP_ITEMS = [
+  {
+    id: 'h1',
+    title: 'Actions',
+    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+  },
+  {
+    id: 'h2',
+    title: 'Workflow?',
+    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+  },
+  {
+    id: 'h3',
+    title: 'Pipelines',
+    text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+  },
+];
+
+function HelpArea() {
+  return (
+    <section data-testid="help-area" id="help-area">
+      {HELP_ITEMS.map((item) => (
+        <HelpBox key={item.id} title={item.title} text={item.text} />
+      ))}
+    </section>
+  );
+}
+
+export default HelpArea;
